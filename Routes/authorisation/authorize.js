14c708f1fa9c4f93d621666ef02cdc9191155d87
unknown <dhawau29@gmail.com>
2024-09-29 09:52:13 +0100

    make changes

--- Routes/authorisation/authorize.js
+++ Routes/authorisation/authorize.js
@@ -4,10 +4,17 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     console.log('Token from header:', token); // Add this line
+    const jwtSecret = process.env.ACCESS_TOKEN_SECRET;
+     if (!token) {
+        console.log('No token provided'); // Debugging line
+        return res.sendStatus(403); // Forbidden if no token
+    }
+    if (!jwtSecret) {
+        console.error('ACCESS_TOKEN_SECRET is not set'); // Debugging line
+        throw new Error("ACCESS_TOKEN_SECRET must have a value");
+    }
 
-    if (!token) return res.sendStatus(403);
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, jwtSecret, (err, user) => {
         if (err) {
             console.error('Token verification failed:', err.message); // Add this line
             return res.sendStatus(403);
